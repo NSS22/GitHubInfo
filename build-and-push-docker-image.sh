ccab6738ae94d24d2c7fabd2f56649e39f7d1cfb
Serhii Nehoda <sneh@ciklum.com>
2021-02-07 20:46:11 +0200

    change build script

--- build-and-push-docker-image.sh
+++ build-and-push-docker-image.sh
@@ -1,6 +1,6 @@
 docker build -t github-info .
 
-docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) 020712779899.dkr.ecr.eu-central-1.amazonaws.com
+$(aws ecr get-login-password --region eu-central-1) 020712779899.dkr.ecr.eu-central-1.amazonaws.com
 aws ecr create-repository --repository-name github-info
 docker tag github-info:latest 020712779899.dkr.ecr.eu-central-1.amazonaws.com/github-info:latest
 docker push 020712779899.dkr.ecr.eu-central-1.amazonaws.com/github-info:latest
