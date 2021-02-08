297d60578dc840479001c20fa9d8f2b5ef399ba9
Serhii Nehoda <sneh@ciklum.com>
2021-02-08 09:40:11 +0200

    add ecr cloudformation script

--- build-and-push-docker-image.sh
+++ build-and-push-docker-image.sh
@@ -1,6 +1,6 @@
 docker build -t github-info .
 
 $(aws ecr get-login-password --region eu-central-1) 020712779899.dkr.ecr.eu-central-1.amazonaws.com
-aws ecr create-repository --repository-name github-info
+aws cloudformation create-stack --stack-name github-info --template-body file://cloudformation/ecr.yaml
 docker tag github-info:latest 020712779899.dkr.ecr.eu-central-1.amazonaws.com/github-info:latest
 docker push 020712779899.dkr.ecr.eu-central-1.amazonaws.com/github-info:latest
